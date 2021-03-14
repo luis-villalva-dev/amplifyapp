bcb142ad07f4325a25eddb54bd0a1b5436b76f60
luis-villalva-dev <luisfer.villalva.developer@gmail.com>
2021-03-13 21:17:40 -0600

    Localized strings for login

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import { I18n } from 'aws-amplify';
+import { strings } from './strings';
+
+//I18n.setLanguage('es');
+I18n.putVocabularies(strings);
 
 function App() {
   return (
@@ -9,6 +14,9 @@ function App() {
       <header>
         <img src={logo} className="App-logo" alt="logo" />
         <h1>We now have Auth!</h1>
+        <p>
+          {I18n.get('appTitle1')} <code>src/App.js</code>{' '}{I18n.get('appTitle2')}
+        </p>
       </header>
       <AmplifySignOut />
     </div>
